c636bdeaccfb2edfe120189be08ac6e8baac0767
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2024-02-08 12:10:06 +0900

    [add]sound,image

--- script.js
+++ script.js
@@ -9,19 +9,23 @@ function attack() {
     const attackDamage = getRandomNumber(10, 20);
     character.hp -= attackDamage;
     displayMessage("あなたは攻撃を仕掛けました！黒ひげに " + attackDamage + " のダメージを与えました！");
+    playSound("atack.mp3"); // 攻撃効果音を再生
     checkResult();
 }
 
 // 逃げる関数
 function runAway() {
     displayMessage("あなたは逃げ出しました！");
-    checkResult();
+    playSound("run.mp3"); // 攻撃効果音を再生
+    changeImage("ikada.png"); // 逃げる画像に変更
+    disableButtons();
 }
 
 // 結果を確認する関数
 function checkResult() {
     if (character.hp <= 0) {
         displayMessage("黒ひげを倒しました！おめでとうございます！");
+        changeImage("homeless_furousya.png"); // 逃げる画像に変更
         disableButtons();
     } else {
         displayMessage("黒ひげの体力: ???");
@@ -43,4 +47,16 @@ function displayMessage(message) {
 function disableButtons() {
     document.querySelector('button:nth-of-type(1)').disabled = true;
     document.querySelector('button:nth-of-type(2)').disabled = true;
+}
+
+
+// 画像を変更する関数
+function changeImage(imagePath) {
+    const imageElement = document.getElementById("character-image");
+    imageElement.src = imagePath;
+}
+
+function playSound(soundFile) {
+    const audio = new Audio(soundFile);
+    audio.play();
 }
