961896e0ba522ad99ae72dc6a2a9e1f40d42b1e2
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2024-02-08 09:46:03 +0900

    [add]テトリス

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const startButton = document.getElementById("startButton");
-    const startScreen = document.getElementById("startScreen");
-
-    startButton.addEventListener("click", function () {
-        startScreen.style.display = "none"; // スタート画面を非表示にする
-        // ゲームのロジックをここに追加
-    });
-});
