1910dac9d242e30b64ed17e5b89a3baa09011dbf
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2024-02-08 11:35:05 +0900

    [add]黒ひげ

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const startButton = document.getElementById("startButton");
-    const startScreen = document.getElementById("startScreen");
+// キャラクターオブジェクトの作成
+const character = {
+    name: "黒ひげ",
+    hp: getRandomNumber(50, 100),
+};
 
-    startButton.addEventListener("click", function () {
-        startScreen.style.display = "none"; // スタート画面を非表示にする
-        // ゲームのロジックをここに追加
-    });
-});
+// 攻撃関数
+function attack() {
+    const attackDamage = getRandomNumber(10, 20);
+    character.hp -= attackDamage;
+    displayMessage("あなたは攻撃を仕掛けました！黒ひげに " + attackDamage + " のダメージを与えました！");
+    checkResult();
+}
+
+// 逃げる関数
+function runAway() {
+    displayMessage("あなたは逃げ出しました！");
+    checkResult();
+}
+
+// 結果を確認する関数
+function checkResult() {
+    if (character.hp <= 0) {
+        displayMessage("黒ひげを倒しました！おめでとうございます！");
+        disableButtons();
+    } else {
+        displayMessage("黒ひげの体力: ???");
+    }
+}
+
+// ランダムな数を返す関数
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// メッセージを表示する関数
+function displayMessage(message) {
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerHTML += "<p>" + message + "</p>";
+}
+
+// ボタンを無効化する関数
+function disableButtons() {
+    document.querySelector('button:nth-of-type(1)').disabled = true;
+    document.querySelector('button:nth-of-type(2)').disabled = true;
+}
